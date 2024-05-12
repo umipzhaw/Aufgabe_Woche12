73ce3331e40c49d180d1a9a25d592ea0fec5643c
umipzhaw <umikerphilipp1993@gmail.com>
2024-05-12 22:35:52 +0200

    änderun server.js automatisch

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello FS2024 DevOps Course_Philipp_Umiker_änderung_Build und Deploy verbunden! :-))))');
+    res.send('Hello FS2024 DevOps Course_Philipp_Umiker_änderung_Build und Deploy verbunden_automatische! :-))))');
 });
 
 app.listen(PORT, HOST);
